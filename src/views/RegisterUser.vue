6cdc1f2dea8460eafd796609a1f0a1b9a03e40ce
Notuc <98197211+Notuc@users.noreply.github.com>
2023-01-28 16:37:59 -0500

    Update RegisterUser.vue

--- src/views/RegisterUser.vue
+++ src/views/RegisterUser.vue
@@ -1,22 +1,33 @@
 <template>
-    <form>
+    <form @submit="handleSubmit">
         <h3>Sign Up</h3>
         <div class="form-group">
             <label>First Name</label>
             <input type="text" class="form-control" placeholder="First Name">
         </div>
+        <br>
         <div class="form-group">
             <label>Last Name </label>
             <input type="text" class="form-control" placeholder="Last Name">
         </div>
+        <br>
         <div class="form-group">
             <label>Email</label>
             <input type="text" class="form-control" placeholder="Email">
         </div>
+        <br>
         <div class="form-group">
             <label>Password</label>
             <input type="text" class="form-control" placeholder="Password">
         </div>
+        <br>
+        <div class="form-group">
+            <label>Confirm Password</label>
+            <input type="text" class="form-control" placeholder="Confirm Password">
+            <br>
+        </div>
+
+        <button class="btn btn-primary btn-block"> Sign Up</button>
        
     </form>
 </template>
