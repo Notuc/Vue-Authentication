52643654f8eeb9c8c5bae133b905eaa9b4da1690
Notuc <98197211+Notuc@users.noreply.github.com>
2023-01-27 22:20:03 -0500

    Added Vue Cli

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-light fixed-top">
+      <div class="container">
+        <a href="#" class="navbar-brand">Home</a>
+        <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a href="#" class="nav-link">login</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Sign Up</a>
+          </li>
+        </ul>
+        </div>
+      </div>
+    </nav>
+  </div>  
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+   
+  }
+}
+</script>
+
+
