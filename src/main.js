d79d1fa889519471b5272245d59f0dad273775bb
Notuc <98197211+Notuc@users.noreply.github.com>
2023-01-29 19:22:01 -0500

    fix code

--- src/main.js
+++ src/main.js
@@ -3,11 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-<<<<<<< Updated upstream
 
 
-=======
->>>>>>> Stashed changes
 createApp(App)
 .use(router)
 .use(store)
